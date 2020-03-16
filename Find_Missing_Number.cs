3fd4379e49adcc867db47662d2e5caf330e09103
battu102 <battu102@gmail.com>
2020-03-16 19:53:03 +0530

    Finding Missing Numbers

---
+++ Find_Missing_Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Find_Missing_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = new int[] { 1, 2, 3, 4, 5, 7, 8, 9, 10 };
+
+           int total = (size.Length+1)*(size.Length+2)/2;
+            int totalSize = 0;
+            for (int i=0; i<=size.Length-1;i++)
+            {
+                totalSize = totalSize + size[i];
+            }
+            int MissingNumber = total - totalSize;
+            Console.WriteLine("The Missing Number is +"+MissingNumber);
+            Console.ReadLine();
+        
+        }
+        
+    }
+}
